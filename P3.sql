b3df5dc7e3b82fa1f69b61b2ade093d1be378b39
thaonguyyen <123501346+thaonguyyen@users.noreply.github.com>
2024-10-18 00:03:11 -0400

    full SQL code

--- P3.sql
+++ P3.sql
@@ -133,7 +133,7 @@ CREATE TABLE Review (
 	review_id BIGINT NOT NULL,
 	destination_id BIGINT NOT NULL,
 	user_id BIGINT NOT NULL,
-	star_rating VARCHAR(5),
+	star_rating INT,
 	comment VARCHAR(200),
 	CONSTRAINT Review_PK PRIMARY KEY (review_id, destination_id),
 	CONSTRAINT Review_Destination_FK FOREIGN KEY (destination_id) REFERENCES Destination(destination_id),
@@ -787,30 +787,90 @@ WITH (
 );
 
 --SQL QUERIES
---return entire trip for a specific user
-SELECT 
-    u.first_name, u.last_name, t.trip_id, t.total_cost, t.number_of_people,
-    i.itinerary_id, d.city AS destination_city, d.state AS destination_state,
-    h.name AS hotel_name, h.price_range AS hotel_price_range, iph.room_type, iph.cost AS hotel_cost,
-    a.name AS activity_name, ipa.duration AS activity_duration,
-    f1.airport_id AS departure_airport, f1.flight_id AS flight_id, 
-    f1.departure_time AS departure_time, f1.arrival_time AS arrival_time, 
-    f1.arrival_airport_id AS arrival_airport, f1.airline AS airline
-FROM 
-    Trip t
-    JOIN [User] u ON t.user_id = u.user_id
-    JOIN Itinerary i ON t.trip_id = i.trip_id
-    JOIN Destination d ON i.destination_id = d.destination_id
-    LEFT JOIN Itinerary_Picked_Hotel iph ON i.itinerary_id = iph.itinerary_id AND i.trip_id = iph.trip_id
-    LEFT JOIN Hotel h ON iph.hotel_id = h.hotel_id
-    LEFT JOIN Itinerary_Picked_Activity ipa ON i.itinerary_id = ipa.itinerary_id AND i.trip_id = ipa.trip_id
-    LEFT JOIN Activity a ON ipa.activity_id = a.activity_id
-    LEFT JOIN Flight f1 ON i.flight_id = f1.flight_id 
-WHERE 
-    u.user_id = 15 -- replace with the specific user_id
-    AND t.trip_id = 7 -- replace with the specific trip_id
-ORDER BY 
-    i.itinerary_id, ipa.activity_id, iph.hotel_id;
+-- How many people are traveling to New York (Aggregate & Subquery & Join)
+SELECT COUNT(*) AS number_traveling_to_new_york
+FROM (
+    SELECT I.trip_id
+    FROM Itinerary I
+    INNER JOIN Destination D ON I.destination_id = D.destination_id
+    WHERE D.[state] = 'New York'
+    GROUP BY I.trip_id
+) AS NY_traveled;
 
+-- Who are all staying hotels with the highest price range (Subquery & Join)
+SELECT DISTINCT u.first_name, u.last_name
+FROM [User] u
+WHERE u.user_id IN (
+    SELECT user_id
+    FROM Trip T
+    WHERE T.trip_id IN (
+        SELECT trip_id 
+        FROM Itinerary I
+        WHERE I.itinerary_id IN (
+            SELECT itinerary_id
+            FROM Itinerary_Picked_Hotel IH
+            INNER JOIN Hotel H ON H.hotel_id = IH.hotel_id
+            WHERE H.price_range = '$$$'
+        )
+    )
+);
 
+-- Get all itineraries for Michael Johnson (Join)
+SELECT * 
+FROM Itinerary I
+INNER JOIN Trip T ON I.trip_id = T.trip_id
+INNER JOIN [User] U ON T.user_id = U.user_id
+WHERE U.first_name = 'Michael' AND U.last_name = 'Johnson';
 
+-- Find the three most expensive chosen hotel (Join)
+SELECT TOP 3 H.hotel_id, H.name, H.price_range, IPH.cost
+FROM Hotel H
+JOIN Itinerary_Picked_Hotel IPH ON IPH.hotel_id = H.hotel_id
+ORDER BY IPH.cost DESC
+
+-- Find the average cost of each hotel chosen (Aggregate & Join)
+SELECT H.hotel_id, H.name, H.price_range, AVG(cost) AS average_cost
+FROM Hotel H
+JOIN Itinerary_Picked_Hotel IPH ON IPH.hotel_id = H.hotel_id
+GROUP BY H.hotel_id, H.name, H.price_range;
+
+-- Find all flights from Texas (Subquery)
+SELECT DISTINCT F.airport_id, F.flight_id, F.departure_time, F.arrival_time, F.airline
+FROM Flight F
+WHERE EXISTS (
+    SELECT 1
+    FROM Airport A
+    WHERE A.[state] = 'Texas'
+    AND F.airport_id = A.airport_id
+);
+
+-- Find all activites in New York City with cost less than $100 (Join)
+SELECT A.name, A.category, DA.cost
+FROM Activity A
+INNER JOIN Destination_Activity DA ON DA.activity_id = A.activity_id
+INNER JOIN Destination D ON DA.destination_id = D.destination_id
+WHERE D.city = 'New York City' AND DA.cost < 100;
+
+-- Find the average rating for each destination (Subquery & Join & Aggregate)
+SELECT RD.city, RD.[state], AVG(RD.star_rating) AS avg_rating
+FROM (
+    SELECT R.destination_id, D.city, D.[state], R.star_rating
+    FROM Review R
+    JOIN Destination D ON D.destination_id = R.destination_id
+) AS RD
+GROUP BY RD.destination_id, RD.city, RD.[state]
+
+-- Get the money spent on trips from each user that has a trip planned (Aggregate & Join)
+SELECT U.user_id, SUM(total_cost) AS total_money_spent
+FROM [User] U
+JOIN Trip T ON T.user_id = U.user_id
+GROUP BY U.user_id;
+
+-- Who spends the most time on activites (Aggregate & Join)
+SELECT TOP 1 U.user_id, U.first_name, U.last_name, SUM(duration) AS total_time_spent
+FROM [User] U
+JOIN Trip T ON T.user_id = U.user_id
+JOIN Itinerary I ON T.trip_id = I.trip_id
+JOIN Itinerary_Picked_Activity IPA ON IPA.itinerary_id = I.itinerary_id
+GROUP BY U.user_id, U.first_name, U.last_name
+ORDER BY SUM(IPA.duration) DESC;
