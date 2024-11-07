9b7adad9c200a2fe55a62dc2e18bc2b535fe364a
thaonguyyen <123501346+thaonguyyen@users.noreply.github.com>
2024-11-07 14:09:59 -0500

    debugged trigger to update total trip cost

--- P4/Trigger.sql
+++ P4/Trigger.sql
@@ -1,38 +1,36 @@
 --trigger to update total trip cost if an itinerary changes
-GO
-CREATE TRIGGER UpdateTripTotalCost
+CREATE OR ALTER TRIGGER UpdateTripTotalCost
 ON Itinerary
 AFTER INSERT, UPDATE, DELETE
 AS
 BEGIN
    SET NOCOUNT ON;
 
-   --handle new itineraries
-   IF EXISTS (SELECT * FROM inserted)
+   -- handle new itineraries (INSERT)
+   IF EXISTS (SELECT * FROM inserted) AND NOT EXISTS (SELECT * FROM deleted)
    BEGIN
-      --update total trip cost by summing all itineraries
       UPDATE Trip
-      SET total_cost = (
-         SELECT SUM(total_cost)
-         FROM Itinerary
-         WHERE Itinerary.trip_id = Trip.trip_id
-      )
+      SET Trip.total_cost = Trip.total_cost + inserted.total_cost
       FROM Trip
       INNER JOIN inserted ON Trip.trip_id = inserted.trip_id;
    END
 
-   --handle deleted itineraries
-   IF EXISTS (SELECT * FROM deleted)
+   -- handle updated itineraries (UPDATE)
+   IF EXISTS (SELECT * FROM inserted) AND EXISTS (SELECT * FROM deleted)
    BEGIN
-      --update trip by summing remaining itineraries
       UPDATE Trip
-      SET total_cost = (
-         SELECT SUM(total_cost)
-         FROM Itinerary
-         WHERE Itinerary.trip_id = Trip.trip_id
-      )
+      SET Trip.total_cost = Trip.total_cost + (inserted.total_cost - deleted.total_cost)
+      FROM Trip
+      INNER JOIN inserted ON Trip.trip_id = inserted.trip_id
+      INNER JOIN deleted ON Trip.trip_id = deleted.trip_id;
+   END
+
+   -- handle deleted itineraries (DELETE)
+   IF EXISTS (SELECT * FROM deleted) AND NOT EXISTS (SELECT * FROM inserted)
+   BEGIN
+      UPDATE Trip
+      SET Trip.total_cost = Trip.total_cost - deleted.total_cost
       FROM Trip
       INNER JOIN deleted ON Trip.trip_id = deleted.trip_id;
    END
 END;
-
