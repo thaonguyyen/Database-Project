283931a11cb16ab08e2a67d7b844fdabbad78133
alliemkim <123777016+alliemkim@users.noreply.github.com>
2024-11-06 19:32:29 -0500

    Added New Function and Procedure

--- P4/Stored-Procedure.sql
+++ P4/Stored-Procedure.sql
@@ -1,8 +1,69 @@
--- PROCEDURES
--- Adds a selected hotel to a user's itinerary. Called when a user chooses a hotel for their trip and updates the Itinerary_Picked_Hotel table to reflect the chosen hotel, room type, and associated costs.
+-- FUNCTIONS
+
+
+-- Calculates the total cost for the given itinerary_id and can be used whenever users make changes to items within that specific itinerary. Keeps the cost updated dynamically.
+CREATE FUNCTION dbo.CalculateItineraryCost
+   (@itinerary_id BIGINT, @trip_id BIGINT)
+RETURNS BIGINT
+AS
+BEGIN
+   DECLARE @itineraryCost BIGINT;
+
+
+   SELECT @itineraryCost =
+       ISNULL(SUM(iph.cost), 0) + ISNULL(SUM(ia.duration * da.cost), 0)
+   FROM Itinerary_Picked_Hotel iph
+   LEFT JOIN Itinerary_Picked_Activity ia ON iph.itinerary_id = ia.itinerary_id AND iph.trip_id = ia.trip_id
+   LEFT JOIN Destination_Activity da ON ia.destination_id = da.destination_id AND ia.activity_id = da.activity_id
+   WHERE iph.itinerary_id = @itinerary_id AND iph.trip_id = @trip_id;
+
+
+   RETURN @itineraryCost;
+END;
 GO
 
 
+-- Calculates the average rating for a given desination
+CREATE FUNCTION dbo.GetAverageRating
+   (@destinationId BIGINT)
+RETURNS FLOAT
+AS
+BEGIN
+   DECLARE @averageRating FLOAT;
+   SELECT @averageRating = AVG(star_rating)
+   FROM Review
+   WHERE destination_id = @destinationId;
+   RETURN @averageRating;
+END;
+GO
+
+
+-- Calculates the total cost of activities for a given itinerary. Users can see how much they will be spending on activities in their itinerary, allowing them to manage their travel budget more effectively.
+CREATE FUNCTION dbo.CalculateTotalActivityCost (@itinerary_id BIGINT)
+RETURNS BIGINT
+AS
+BEGIN
+   DECLARE @totalActivityCost BIGINT;
+
+
+   SELECT @totalActivityCost = ISNULL(SUM(ia.duration * da.cost), 0)
+   FROM Itinerary_Picked_Activity ia
+   JOIN Destination_Activity da ON ia.activity_id = da.activity_id AND ia.destination_id = da.destination_id
+   WHERE ia.itinerary_id = @itinerary_id;
+
+
+   RETURN @totalActivityCost;
+END;
+GO
+
+
+
+
+-- PROCEDURES
+
+
+-- Adds a selected hotel to a user's itinerary. Called when a user chooses a hotel for their trip and updates the Itinerary_Picked_Hotel table to reflect the chosen hotel, room type, and associated costs.
+GO
 CREATE PROCEDURE AddHotelToItinerary
    @itinerary_id BIGINT,
    @trip_id BIGINT,
@@ -14,7 +75,7 @@ AS
 BEGIN
    INSERT INTO Itinerary_Picked_Hotel (itinerary_id, trip_id, destination_id, hotel_id, room_type, cost)
    VALUES (@itinerary_id, @trip_id, @destination_id, @hotel_id, @room_type, @cost);
-END
+END;
 GO
 
 
@@ -28,44 +89,31 @@ BEGIN
    JOIN Itinerary_Picked_Hotel iph ON i.itinerary_id = iph.itinerary_id AND i.trip_id = iph.trip_id
    JOIN Destination d ON i.destination_id = d.destination_id
    WHERE i.trip_id = @trip_id;
-END
-
-
+END;
 GO
 
 
--- FUNCTIONS
--- Calculates the total cost of a trip based on selected hotels and activites. Called whenever a user adds or removes items from their itinerary to dynamically update the total trip cost.
-CREATE FUNCTION CalculateTotalCost
-   (@trip_id BIGINT)
-RETURNS BIGINT
+-- Calculates and updates the total costs for each itinerary for a specified trip and then aggregates these costs to update the overall cost of the trip itself in the Trip table
+CREATE PROCEDURE TotalCostForTrip
+   @trip_id BIGINT
 AS
 BEGIN
-   DECLARE @totalCost BIGINT;
+   DECLARE @tripTotalCost BIGINT = 0;
 
 
-   SELECT @totalCost = ISNULL(SUM(iph.cost), 0) + ISNULL(SUM(ia.duration * da.cost), 0)
-   FROM Itinerary_Picked_Hotel iph
-   LEFT JOIN Itinerary_Picked_Activity ia ON iph.trip_id = ia.trip_id
-   LEFT JOIN Destination_Activity da ON ia.destination_id = da.destination_id AND ia.activity_id = da.activity_id
-   WHERE iph.trip_id = @trip_id;
+   UPDATE Itinerary
+   SET total_cost = dbo.CalculateItineraryCost(itinerary_id, @trip_id)
+   WHERE trip_id = @trip_id;
 
 
-   RETURN @totalCost;
-END
+   SELECT @tripTotalCost = SUM(total_cost)
+   FROM Itinerary
+   WHERE trip_id = @trip_id;
 
 
+   UPDATE Trip
+   SET total_cost = @tripTotalCost
+   WHERE trip_id = @trip_id;
+END;
 GO
--- Calculates the average rating for a given desination
-CREATE FUNCTION GetAverageRating
-   (@destinationId BIGINT)
-RETURNS FLOAT
-AS
-BEGIN
-   DECLARE @averageRating FLOAT;
-   SELECT @averageRating = AVG(star_rating)
-   FROM Review
-   WHERE destination_id = @destinationId;
-   RETURN @averageRating;
-END
-GO
+
