d0408008904a3c1bfb6f488f6d78f226835874fe
kevinlam0 <kevinclam02@gmail.com>
2024-11-07 11:07:43 -0500

    Added new procedure

--- P4/Stored-Procedure.sql
+++ P4/Stored-Procedure.sql
@@ -16,18 +16,60 @@ GO
 
 
 -- Retrieves all details related to a specific trip, including destinations, hotels, activities, and total costs. Allows users to review their trip details before finalizing plans.
-CREATE PROCEDURE GetTripDetails
+-- CREATE PROCEDURE GetTripDetails
+--    @trip_id BIGINT
+-- AS
+-- BEGIN
+--    SELECT *
+--    FROM Itinerary i
+--    JOIN Itinerary_Picked_Hotel iph ON i.itinerary_id = iph.itinerary_id AND i.trip_id = iph.trip_id
+--    JOIN Destination d ON i.destination_id = d.destination_id
+--    WHERE i.trip_id = @trip_id;
+-- END;
+-- GO
+
+-- Delete a selected hotel from a user's itinerary. Called when a user decides to remove a hotel from their trip and updates the Itinerary_Picked_Hotel table to reflect the change
+CREATE PROCEDURE DeleteHotelFromItinerary
+   @itinerary_id BIGINT,
    @trip_id BIGINT
 AS
 BEGIN
-   SELECT *
-   FROM Itinerary i
-   JOIN Itinerary_Picked_Hotel iph ON i.itinerary_id = iph.itinerary_id AND i.trip_id = iph.trip_id
-   JOIN Destination d ON i.destination_id = d.destination_id
-   WHERE i.trip_id = @trip_id;
+   -- Check for existence of this hotel reservation
+   IF NOT EXISTS (SELECT 1 FROM Itinerary_Picked_Hotel WHERE itinerary_id = @itinerary_id AND trip_id = @trip_id)
+   BEGIN
+      RAISERROR('Hotel does not exist in the itinerary', 16, 1);
+      RETURN;
+   END;
+
+   -- Get the hotel cost
+   DECLARE @hotel_cost BIGINT
+   SELECT @hotel_cost = cost
+   FROM Itinerary_Picked_Hotel
+   WHERE itinerary_id = @itinerary_id AND trip_id = @trip_id;
+
+   -- Check if the total_cost would be negative for the Itinerary or Trip
+   IF (SELECT total_cost - @hotel_cost FROM Itinerary WHERE itinerary_id = @itinerary_id) < 0 OR
+      (SELECT total_cost - @hotel_cost FROM Trip WHERE trip_id = @trip_id) < 0
+   BEGIN
+      RAISERROR('Subtracting this hotel cost would result in a negative total cost', 16, 1);
+      RETURN;
+   END;
+
+   -- Subtracting total cost in itinerary
+   Update Itinerary
+   SET total_cost = total_cost - @hotel_cost
+   WHERE itinerary_id = @itinerary_id AND trip_id = @trip_id;
+
+   -- Subtracting total cost in trip
+   Update Trip
+   SET total_cost = total_cost - @hotel_cost
+   WHERE trip_id = @trip_id;
+
+   -- Delete from the picked table 
+   DELETE FROM Itinerary_Picked_Hotel
+   WHERE itinerary_id = @itinerary_id AND trip_id = @trip_id;
 END;
 GO
-
 
 -- Calculates and updates the total costs for each itinerary for a specified trip and then aggregates these costs to update the overall cost of the trip itself in the Trip table
 CREATE PROCEDURE TotalCostForTrip
@@ -50,4 +92,3 @@ BEGIN
    WHERE trip_id = @trip_id;
 END;
 GO
-
